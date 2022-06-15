9ff39804eecbe559406c7d486ef3399cf3f04c40
unknown <trang.lehuyen@hcmut.edu.vn>
2022-06-15 16:59:14 +0700

    feature/student-crud-lhtrang route2

---
+++ database/migrations/2022_06_15_013613_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class StudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('student_code');
+            $table->string('departure');
+            $table->string('faculty');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- database/migrations/2022_06_15_014317_add_collumn_to_users_table.php
+++ database/migrations/2022_06_15_014317_add_collumn_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class AddCollumnToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -31,6 +31,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
